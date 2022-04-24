ea323a2422f162ed122916aeacefe17f3037dd9d
Matt Obee <github@mattobee.com>
2022-04-24 12:52:42 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/scripts/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist/assets'),
+    path: path.resolve(__dirname, 'dist/scripts'),
     filename: 'main.js'
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            }
-          },
-          {
-            loader: 'postcss-loader'
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
-        ]
+        }
       }
     ]
   }
-};
+}
