a112fb623a8fcadd9de21f21931c033c7becb9c2
Matt Obee <github@mattobee.com>
2022-04-26 08:17:55 +0100

    Add meta file for any global data

---
+++ src/_data/meta.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // url: process.env.URL || "http://localhost:4000",
+    siteName: "Eventua11y",
+    siteDescription: "",
+    authorName: "Matt Obee",
+    twitterUsername: "mattobee", // no `@`
+  };
