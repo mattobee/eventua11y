f3c877248f932be926c4f6eefece137c69795744
Matt Obee <github@mattobee.com>
2022-05-02 17:37:03 +0100

    Format event start date based on user's locale

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/images");
+
+    // Format event dates based on user's locale
+    eleventyConfig.addFilter("eventDate", (dateObj) => {
+      return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATETIME_FULL);
+    });
+    
     return {
       dir: {
         input: "src",
