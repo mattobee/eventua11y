7f190ecd18c72de32a17a2451f9cd9112a7a6fc7
Matt Obee <github@mattobee.com>
2023-03-09 12:55:51 +0000

    Simplify dependencies
    Kill webpack et al in favour of compiling CSS and Sass with a single Eleventy plugin.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const { DateTime } = require("luxon");
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
-module.exports = function(eleventyConfig) {
+module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy("./src/images");
+    eleventyConfig.addPlugin(eleventySass);
 
     // Format event dates based on user's locale
     eleventyConfig.addFilter("eventDate", (dateObj) => {
