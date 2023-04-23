216387fe23ff0ec1ce5324869f37fd722f30d3ed
Matt Obee <matt@obee.me>
2023-04-23 21:28:57 +0100

    Apply a readable format to dates

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -1,4 +1,6 @@
 const dayjs = require("dayjs");
+const localizedFormat = require('dayjs/plugin/localizedFormat');
+dayjs.extend(localizedFormat);
 
 function readableDate(dateObj) {
   return new Date(dateObj).toDateString();
