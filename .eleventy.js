bb93fea86b1c86ba917e6c5807f417b19161ece9
Matt Obee <matt@obee.me>
2023-08-01 11:47:49 +0100

    Use Eleventy Edge to display today's date

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 const filters = require("./src/_11ty/filters");
+const { EleventyEdgePlugin } = require("@11ty/eleventy");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPlugin(EleventyEdgePlugin);
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addShortcode("today", () => `${new Date()}`);
+
 
   // Create filters
   Object.keys(filters).forEach((filterName) => {
