415df2c7d7943034c15b0987ca300c0dd96d0d9a
Matt Obee <matt@obee.me>
2023-08-01 13:27:10 +0100

    Remove time from today's date

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -17,15 +17,19 @@ export default async (request, context) => {
     const locale = request.headers["accept-language"] || "en-GB";
     const { timezone } = context.geo;
     const date = new Date();
-    const localTimeNow = date.toLocaleString(locale, {
-      timeZone: timezone
+    const localDateNow = date.toLocaleDateString(locale, {
+      timeZone: timezone,
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     });
 
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
       // e.g. Fancier json output
       // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
-      eleventyConfig.addShortcode("today", () => localTimeNow);
+      eleventyConfig.addShortcode("today", () => localDateNow);
     });
 
     return await edge.handleResponse();
