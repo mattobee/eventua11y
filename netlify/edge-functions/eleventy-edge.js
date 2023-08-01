300fbd224072662de0c3dd6458444db7a7b09f42
Matt Obee <matt@obee.me>
2023-08-01 16:42:09 +0100

    Add events to global data in edge

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -3,6 +3,8 @@ import {
   precompiledAppData,
 } from "./_generated/eleventy-edge-app.js";
 
+import events from "../../src/_data/events.json" assert {type: 'json'};
+
 export default async (request, context) => {
   try {
     let edge = new EleventyEdge("edge", {
@@ -28,6 +30,27 @@ export default async (request, context) => {
       // Add some custom Edge-specific configuration
       // e.g. Fancier json output
       // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+
+      // Add events to global data in edge
+      eleventyConfig.addGlobalData("events", events);
+
+      /* Converts the given date string to ISO8601 format. */
+      eleventyConfig.addFilter("isoDate", function(date) {
+        const dateObject = new Date(date);
+        return dateObject.toISOString();
+      });
+
+      // Return events taking place today, based on locale
+      eleventyConfig.addFilter("todaysEvents", function(events) {
+        const today = new Date().toISOString().slice(0, 10);
+        return events.filter((event) => {
+          const eventDateStart = new Date(event.dateStart).toISOString().slice(0, 10);
+          const eventDateEnd = new Date(event.dateEnd).toISOString().slice(0, 10);
+          return eventDateStart <= today && today <= eventDateEnd;
+        });
+      });
+
+      // Return today's date based on locale
       eleventyConfig.addShortcode("today", () => localDateNow);
     });
 
