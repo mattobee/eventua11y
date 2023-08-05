c55277c43f121dc1cebad1a3e3f23e7adf12c544
Matt Obee <matt@obee.me>
2023-08-05 23:27:59 +0100

    Remove unused filters

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -1,19 +1,7 @@
-const dayjs = require("dayjs");
-const localizedFormat = require("dayjs/plugin/localizedFormat");
-dayjs.extend(localizedFormat);
-
 function readableDate(dateObj) {
   return new Date(dateObj).toDateString();
 }
 
-/* Converts the given date string to ISO8601 format. */
-const isoDate = (date) => dayjs(date).toISOString();
-
-/* Formats a date using Day.js */
-const formatDate = (date, format) => dayjs(date).format(format);
-
 module.exports = {
-  readableDate,
-  isoDate,
-  formatDate
+  readableDate
 };
