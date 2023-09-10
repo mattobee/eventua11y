ffa963acef3d2a473cf3567d04a296073c1263e2
Matt Obee <matt@obee.me>
2023-09-10 22:57:09 +0100

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,6 +2,5 @@ module.exports = {
     // url: process.env.URL || "http://localhost:4000",
     siteName: "Eventua11y",
     siteDescription: "",
-    authorName: "Matt Obee",
-    twitterUsername: "mattobee", // no `@`
+    authorName: "Matt Obee"
   };
