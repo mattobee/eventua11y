23c61ae78d99f59115b7d75fe4d201792114ace9
Matt Obee <matt@obee.me>
2023-09-26 22:08:10 +0100

    Delete old envar in _data

--- src/_data/envar.js
+++
@@ -1,8 +0,0 @@
-const envar = {
-    sanityProjectId: Deno.env.get("SANITY_PROJECT"),
-    sanityToken: Deno.env.get("SANITY_TOKEN"),
-    sanityDataset: Deno.env.get("SANITY_DATASET") || "production",
-    sanityApiVersion: Deno.env.get("SANITY_API_VERSION") || "2023-09-04",
-  };
-  
-  export default envar;
