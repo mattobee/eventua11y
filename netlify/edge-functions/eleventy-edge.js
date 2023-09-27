ed9dc3ebf5839f2d3e582f9f27c321aff1f4b00f
Matt Obee <matt@obee.me>
2023-09-27 23:23:48 +0100

    Update eleventy-edge.js

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -1,8 +1,4 @@
-import {
-  EleventyEdge,
-  precompiledAppData,
-} from "./_generated/eleventy-edge-app.js";
-
+import { EleventyEdge, precompiledAppData } from "./_generated/eleventy-edge-app.js";
 import getEvents from "./sanity.js";
 import dayjs from "./day.js";
 import meta from "../../src/_data/meta.mjs";
@@ -24,15 +20,14 @@ export default async (request, context) => {
     // Set the locale based on the request headers, defaulting to en-gb
     const LOCALE = request.headers["accept-language"] || "en-gb";
     console.log("LOCALE is " + LOCALE);
-    
+
     const { timezone } = context.geo;
     const now = new dayjs();
-    
-    console.log("now is " + now)
-    console.log("timezone is " + timezone)
+
+    console.log("now is " + now);
+    console.log("timezone is " + timezone);
 
     edge.config((eleventyConfig) => {
-
       // Make Eleventy global data available on the edge
       eleventyConfig.addGlobalData("events", events); // Events from Sanity
       eleventyConfig.addGlobalData("meta", meta); // Meta data
@@ -40,54 +35,60 @@ export default async (request, context) => {
       /* Converts the given date string to ISO8601 format. */
       eleventyConfig.addFilter("isoDate", (date) => dayjs(date).toISOString());
 
-
-       /* Formats the given date string based on the user's locale. */
-       eleventyConfig.addFilter("localizedDate", (date) =>
+      /* Formats the given date string based on the user's locale. */
+      eleventyConfig.addFilter("localizedDate", (date) =>
         dayjs(date).locale(LOCALE).tz(timezone).format("LLL")
       );
 
       // Return theme events taking place today, based on locale
-      eleventyConfig.addFilter("todaysThemes", function(events) {
+      eleventyConfig.addFilter("todaysThemes", function (events) {
         return events.filter((event) => {
           // console.log(event)
           const eventDateStart = new dayjs(event.dateStart);
           const eventDateEnd = new dayjs(event.dateEnd);
-          return eventDateStart.isSameOrBefore(now) && eventDateEnd.isSameOrAfter(now) && event.type == "theme";
+          return (
+            eventDateStart.isSameOrBefore(now) &&
+            eventDateEnd.isSameOrAfter(now) &&
+            event.type == "theme"
+          );
         });
       });
 
       // Return non-theme events taking place today, based on locale
-      eleventyConfig.addFilter("todaysEvents", function(events) {
+      eleventyConfig.addFilter("todaysEvents", function (events) {
         return events.filter((event) => {
           const eventDateStart = new dayjs(event.dateStart);
           const eventDateEnd = new dayjs(event.dateEnd);
           // Return an event if it starts today or earlier, and ends today or later
-          return eventDateStart.isSameOrBefore(now) && eventDateEnd.isSameOrAfter(now) && event.type != "theme";
+          return (
+            eventDateStart.isSameOrBefore(now) &&
+            eventDateEnd.isSameOrAfter(now) &&
+            event.type != "theme"
+          );
         });
       });
 
       /* Returns a list of upcoming events in chronological order */
-      eleventyConfig.addFilter("upcomingEvents", function(events) {
+      eleventyConfig.addFilter("upcomingEvents", function (events) {
         return events.filter((event) => {
-            return new dayjs(event.dateStart).isAfter(now, 'day');
-          })
-          // .reverse();
+          return new dayjs(event.dateStart).isAfter(now, "day");
+        });
+        // .reverse();
       });
 
       // Return today's date as an iso string
       eleventyConfig.addShortcode("todayISO", () => now.toISOString());
 
       // Return today's date as a locale string
-      eleventyConfig.addShortcode("today", function() {
+      eleventyConfig.addShortcode("today", function () {
         return dayjs(now).locale(LOCALE).tz(timezone).format("LL");
         // return now;
       });
 
       // Return the user's timezone
-      eleventyConfig.addShortcode("timezone", function(context) {
+      eleventyConfig.addShortcode("timezone", function (context) {
         return timezone;
       });
-
     });
 
     return await edge.handleResponse();
