15012d8d44283f3443ec8036ff3c44b585fa7470
Matt Obee <matt@obee.me>
2023-11-08 21:50:55 +0000

    New filter to expand timezone abbreviations

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -114,6 +114,46 @@ export default async (request, context) => {
         return months;
       });
 
+      eleventyConfig.addFilter("expandTimezone", function (abbreviation) {
+        const timezoneNames = {
+          "GMT": "Greenwich Mean Time",
+          "GMT+1": "Greenwich Mean Time +1",
+          "UTC": "Coordinated Universal Time",
+          "BST": "British Summer Time",
+          "CET": "Central European Time",
+          "CEST": "Central European Summer Time",
+          "EET": "Eastern European Time",
+          "EEST": "Eastern European Summer Time",
+          "MSK": "Moscow Time",
+          "MSD": "Moscow Summer Time",
+          "IST": "Indian Standard Time",
+          "SGT": "Singapore Time",
+          "JST": "Japan Standard Time",
+          "AEST": "Australian Eastern Standard Time",
+          "AEDT": "Australian Eastern Daylight Time",
+          "NZST": "New Zealand Standard Time",
+          "NZDT": "New Zealand Daylight Time",
+          "PST": "Pacific Standard Time",
+          "PDT": "Pacific Daylight Time",
+          "MST": "Mountain Standard Time",
+          "MDT": "Mountain Daylight Time",
+          "CST": "Central Standard Time",
+          "CDT": "Central Daylight Time",
+          "EST": "Eastern Standard Time",
+          "EDT": "Eastern Daylight Time",
+          "AKST": "Alaska Standard Time",
+          "AKDT": "Alaska Daylight Time",
+          "HST": "Hawaii Standard Time",
+          "HAST": "Hawaii-Aleutian Standard Time",
+          "HADT": "Hawaii-Aleutian Daylight Time",
+          "SST": "Samoa Standard Time",
+          "SDT": "Samoa Daylight Time",
+          "CHST": "Chamorro Standard Time",
+          "CHDT": "Chamorro Daylight Time"
+        };
+        return timezoneNames[abbreviation] || abbreviation;
+      });
+
       // Return today's date as an iso string
       eleventyConfig.addShortcode("todayISO", () => now.toISOString());
 
