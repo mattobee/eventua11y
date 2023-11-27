4d190933f19baeb16ed71d44d78d67490aa79835
Matt Obee <matt@obee.me>
2023-11-27 21:32:46 +0000

    Add playwright-axe

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,10 +1,20 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { AxeBuilder } = require('axe-playwright');
 
 test('has title', async ({ page, baseURL }) => {
   await page.goto(baseURL);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Eventua11y/);
+
 });
 
+test('has no accessibility violations', async ({ page, baseURL }) => {
+  await page.goto(baseURL);
+
+  const builder = new AxeBuilder({ page });
+  const results = await builder.analyze();
+
+  expect(results.violations).toHaveLength(0);
+});
