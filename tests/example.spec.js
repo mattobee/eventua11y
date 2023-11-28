7be9be7a7e98d9b01f8900e3d798f69c9f35756f
Matt Obee <matt@obee.me>
2023-11-28 15:05:14 +0000

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,16 +2,16 @@
 const { test, expect } = require('@playwright/test');
 const AxeBuilder = require('@axe-core/playwright').default;
 
-test('has title', async ({ page, baseURL }) => {
+test.beforeEach(async ({ page, baseURL }) => {
   await page.goto(baseURL);
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Eventua11y/);
-
 });
 
-test('has no accessibility violations', async ({ page, baseURL }) => {
-  await page.goto(baseURL);
+test('has title', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Eventua11y/);
+});
+
+test('has no accessibility violations', async ({ page }) => {
   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
   expect(accessibilityScanResults.violations).toEqual([]);
 });
