1e5de9d704fc1126df6e22e1e95ebcb72a11ae4c
Matt Obee <matt@obee.me>
2023-12-05 22:45:50 +0000

    Hide empty months

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,6 +42,7 @@ document.addEventListener("alpine:init", () => {
   Alpine.store("filters").filterEvents = () => {
     // Select all events on the page
     const events = document.querySelectorAll(".event");
+    const monthSections = document.querySelectorAll(".month");
 
     // Loop over the events
     events.forEach((event) => {
@@ -53,23 +54,38 @@ document.addEventListener("alpine:init", () => {
       // If the 'themes' filter is false and the event type is 'theme', hide the event
       // If the 'themes' filter is true and the event type is 'theme', show the event
       if (eventType === "theme") {
-        event.style.display = Alpine.store("filters").themes ? "" : "none";
+        event.hidden = !Alpine.store("filters").themes;
       } else {
         // Check if the event matches the attendance mode filter
-        const matchesAttendanceMode = 
-          (!Alpine.store("filters").attendanceOnline && !Alpine.store("filters").attendanceOffline) ||
-          (Alpine.store("filters").attendanceOnline && (eventAttendanceMode === "online" || eventAttendanceMode === "mixed")) ||
-          (Alpine.store("filters").attendanceOffline && (eventAttendanceMode === "offline" || eventAttendanceMode === "mixed"));
+        const matchesAttendanceMode =
+          (!Alpine.store("filters").attendanceOnline &&
+            !Alpine.store("filters").attendanceOffline) ||
+          (Alpine.store("filters").attendanceOnline &&
+            (eventAttendanceMode === "online" ||
+              eventAttendanceMode === "mixed")) ||
+          (Alpine.store("filters").attendanceOffline &&
+            (eventAttendanceMode === "offline" ||
+              eventAttendanceMode === "mixed"));
 
         // Check if the event matches the cfs filter
-        const matchesCfs = 
-          (!Alpine.store("filters").cfsOpen && !Alpine.store("filters").cfsClosed) ||
+        const matchesCfs =
+          (!Alpine.store("filters").cfsOpen &&
+            !Alpine.store("filters").cfsClosed) ||
           (Alpine.store("filters").cfsOpen && eventCfsStatus) ||
           (Alpine.store("filters").cfsClosed && !eventCfsStatus);
 
         // If the event matches all filter criteria, show the event; otherwise, hide it
-        event.style.display = (matchesAttendanceMode && matchesCfs) ? "" : "none";
+        event.hidden = !(matchesAttendanceMode && matchesCfs);
       }
+
+      // After filtering the events, check each month section
+      monthSections.forEach((section) => {
+        // Select all visible events in the section
+        const visibleEvents = section.querySelectorAll(".event:not([hidden])");
+
+        // If there are no visible events in the section, hide the section
+        section.hidden = visibleEvents.length === 0;
+      });
     });
   };
 });
