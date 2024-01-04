eae1bfad38f91bdb34046a6cb5454f98e66ad1c3
Matt Obee <matt@obee.me>
2024-01-04 11:00:45 +0000

    Update dependencies for Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { sanityIntegration } from '@sanity/astro';
 import { defineConfig } from "astro/config";
-
 import netlify from "@astrojs/netlify/functions";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: netlify(),
-  integrations: [
-    sanityIntegration({
-      projectId: "2g5zqxo3",
-      dataset: "production",
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-    })
-  ]
+  integrations: [sanityIntegration({
+    projectId: "2g5zqxo3",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false
+  }), sanity()]
 });
