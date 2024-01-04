bf6583a72fe9fa95415edfe1935072cb6436b146
Matt Obee <matt@obee.me>
2024-01-04 11:06:34 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { sanityIntegration } from '@sanity/astro';
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
-import sanity from "@sanity/astro";
-
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
@@ -13,5 +11,5 @@ export default defineConfig({
     dataset: "production",
     // Set useCdn to false if you're building statically.
     useCdn: false
-  }), sanity()]
+  })]
 });
