0e2eb427fefdf90233ce5302c1b6886ed5e401e9
Matt Obee <matt@obee.me>
2024-03-17 20:45:01 +0000

    Remove edge functions

--- netlify/edge-functions/envar.js
+++
@@ -1,8 +0,0 @@
-const getEnvar = () => ({
-  SANITY_PROJECT_ID: Deno.env.get("SANITY_PROJECT"),
-  SANITY_TOKEN: Deno.env.get("SANITY_TOKEN"),
-  SANITY_DATASET: Deno.env.get("SANITY_DATASET") || "production",
-  SANITY_API_VERSION: Deno.env.get("SANITY_API_VERSION") || "2023-09-04"
-});
-
-export default getEnvar;
